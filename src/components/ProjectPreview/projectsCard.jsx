1d4f07373d5edf9f0944bbc2ae4c15208f17264d
GitThomasP <Thomasjensen990@gmail.com>
2024-11-19 13:17:13 +0100

    Update projectsCard.jsx

--- src/components/ProjectPreview/projectsCard.jsx
+++ src/components/ProjectPreview/projectsCard.jsx
@@ -1,7 +1,7 @@
 export default function projectsCard(img, title, description) {
   return (
     <>  
-      <img src={img}></img>
+      <img src={img}>Hej</img>
       <h2>{title}</h2>
       <p>{description}</p>
     </>
