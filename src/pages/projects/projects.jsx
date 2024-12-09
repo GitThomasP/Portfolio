53f8c2eea5a2c04f01abce1adf4c0d545acafb02
GitThomasP <Thomasjensen990@gmail.com>
2024-12-10 00:16:44 +0100

    Work on projects page

--- src/pages/projects/projects.jsx
+++ src/pages/projects/projects.jsx
@@ -6,20 +6,127 @@ import projectsCard from "../../components/ProjectPreview/projectsCard.jsx";
 export default function projects() {
   const [selectedProject, setSelectedProject] = useState();
 
-  
   function handlerSelectProject() {
     setSelectedProject();
   }
   return (
     <section>
-      <div>  
-        {PROJECTS.map((projectCard) => (
-            <projectsCard key={projectCard} title={projectCard.title} img={projectCard.image} description={projectCard.description}/>
-        ))}
+      <h2>Projects</h2>
+      <div className="container flex-row">
+        <div className='card-container'>
+          <a href='/' className='hero-image-container'>
+            <img
+              className='hero-image'
+              src='https://i.postimg.cc/NfR2yhNs/image-equilibrium.jpg'
+              alt='Spinning glass cube'
+            />
+          </a>
+          <main className='main-content'>
+            <h1>
+              Equilibrium #3429
+            </h1>
+            <p>Our Equilibrium collection promotes balance and calm.</p>
             
-      </div>
-      <div>
-        <button onClick={handlerSelectProject}></button>
+          </main>
+          <div className='card-attribute'>
+            <img
+              src='https://i.postimg.cc/SQBzNQf1/image-avatar.png'
+              alt='avatar'
+              className='small-avatar'
+            />
+            <img
+              src='https://i.postimg.cc/SQBzNQf1/image-avatar.png'
+              alt='avatar'
+              className='small-avatar'
+            />
+          </div>
+        </div>
+
+        <div className='card-container'>
+          <a href='/' className='hero-image-container'>
+            <img
+              className='hero-image'
+              src='https://i.postimg.cc/NfR2yhNs/image-equilibrium.jpg'
+              alt='Spinning glass cube'
+            />
+          </a>
+          <main className='main-content'>
+            <h1>
+              Equilibrium #3429
+            </h1>
+            <p>Our Equilibrium collection promotes balance and calm.</p>
+          </main>
+          <div className='card-attribute'>
+            <img
+              src='https://i.postimg.cc/SQBzNQf1/image-avatar.png'
+              alt='avatar'
+              className='small-avatar'
+            />
+            <img
+              src='https://i.postimg.cc/SQBzNQf1/image-avatar.png'
+              alt='avatar'
+              className='small-avatar'
+            />
+          </div>
+        </div>
+
+        <div className='card-container'>
+          <a href='/' className='hero-image-container'>
+            <img
+              className='hero-image'
+              src='https://i.postimg.cc/NfR2yhNs/image-equilibrium.jpg'
+              alt='Spinning glass cube'
+            />
+          </a>
+          <main className='main-content'>
+            <h1>
+              Equilibrium #3429
+            </h1>
+            <p>Our Equilibrium collection promotes balance and calm.</p>
+          </main>
+          <div className='card-attribute'>
+            <img
+              src='https://i.postimg.cc/SQBzNQf1/image-avatar.png'
+              alt='avatar'
+              className='small-avatar'
+            />
+            <img
+              src='https://i.postimg.cc/SQBzNQf1/image-avatar.png'
+              alt='avatar'
+              className='small-avatar'
+            />
+          </div>
+        </div>
+
+        <div className='card-container'>
+          <a href='/' className='hero-image-container'>
+            <img
+              className='hero-image'
+              src='https://i.postimg.cc/NfR2yhNs/image-equilibrium.jpg'
+              alt='Spinning glass cube'
+            />
+          </a>
+          <main className='main-content'>
+            <h1>
+              Equilibrium #3429
+            </h1>
+            <p>Our Equilibrium collection promotes balance and calm.</p>
+          </main>
+          <div className='card-attribute'>
+            <img
+              src='https://i.postimg.cc/SQBzNQf1/image-avatar.png'
+              alt='avatar'
+              className='small-avatar'
+            />
+            <img
+              src='https://i.postimg.cc/SQBzNQf1/image-avatar.png'
+              alt='avatar'
+              className='small-avatar'
+            />
+          </div>
+        </div>
+
+        
       </div>
     </section>
   );
